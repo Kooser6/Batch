690b1b0a1eb98e0b047ed7951c69cc911d35cabf
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-10-13 12:38:45 -0400

    Update Evaluator.php

--- src/Batch/Evaluator.php
+++ src/Batch/Evaluator.php
@@ -1,1 +1,19 @@
+<?php
+declare(strict_types=1);
 
+namespace Phoenix\Batch;
+
+/**
+ * Defines an evaluator.
+ */
+interface Evaluator
+{
+    /**
+     * Evaluate the position.
+     *
+     * @param Position $position The position to evaluate.
+     *
+     * @return int Returns the evaluation sum of the position.
+     */
+    public function evaluate(Position $position): int;
+}
