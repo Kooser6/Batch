a0a1667a2b1f647bce3020e1280d5c1553f4c1d1
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-10-13 16:11:17 -0400

    Enhance.

--- src/Evaluation/MaterialEvaluation.php
+++ src/Evaluation/MaterialEvaluation.php
@@ -11,21 +11,21 @@ use Phoenix\Batch\Position;
 final class MaterialEvaluation implements Evaluator
 {
     /** @var array $pieceValues A list of values associated with the pieces. */
-    private $pieceValues = [
+    private static $pieceValues = [
         [
             'K' => 10000,
             'Q' => 1000,
             'R' => 525,
-            'B' => 400,
-            'N' => 300,
+            'B' => 425,
+            'N' => 325,
             'P' => 100,
         ],
         [
             'K' => -10000,
             'Q' => -1000,
             'R' => -525,
-            'B' => -400,
-            'N' => -300,
+            'B' => -425,
+            'N' => -325,
             'P' => -100,
         ],
     ];
@@ -35,20 +35,28 @@ final class MaterialEvaluation implements Evaluator
      */
     public function evaluate(Position $position): int
     {
+        Game::importData($position->getExportData);
         $sum = 0;
-        foreach ($position->pieces() as $key => $data) {
-            $piece = $key;
-            if ($position->inactive($piece)) {
-                continue;
-            }
-            $piece = substr($piece, 1, 1);
-            $location = $data['location'];
-            $color = $data['color'];
-            if ($color == 'R' || $color == 'Y') {
-                $sum += $this->pieceValues[0][$piece];
+        foreach (Utils::$pieces as $pieceKey => $pieceData) {
+            $color = substr($pieceKey, 0, 1);
+            $piece = substr($pieceKey, 1, 1);
+            if (is_array($pieceData)) {
+                $type = $pieceData[1];
+                if ($color == 'R' || $color == 'Y') {
+                    $value = self::$pieceValues[0][$type];
+                } else {
+                    $value = self::$pieceValues[1][$type];
+                }
+            } elseif (is_string($pieceData)) {
+                if ($color == 'R' || $color == 'Y') {
+                    $value = self::$pieceValues[0][$piece];
+                } else {
+                    $value = self::$pieceValues[1][$piece];
+                }
             } else {
-                $sum += $this->pieceValues[1][$piece];
+                $value = 0;
             }
+            $sum += $value;
         }
         return $sum;
     }
