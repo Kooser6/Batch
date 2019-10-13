9d84ab4ce5f12b5e0f5b748f98afa784cbb131ef
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-10-13 14:45:00 -0400

    Add logic.

--- src/Batch/Algorithm.php
+++ src/Batch/Algorithm.php
@@ -4,8 +4,56 @@ declare(strict_types=1);
 namespace Phoenix\Batch;
 
 /**
- * Defines an algorithm.
+ * The algorithm.
  */
 interface Algorithm
 {
+    /**
+     * Construct a new minimax algorithm.
+     *
+     * @param Evaluator $evaluator The evaluator.
+     * @param int       $depth     The search depth.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(Evaluator $evaluator, int $depth = 3);
+
+    /**
+     * Set the evaluator.
+     *
+     * @param Evaluator $evaluator The evaluator.
+     *
+     * @return Algorithm Returns the minimax algorithm.
+     */
+    public function setEvaluator(Evaluator $evaluator): Algorithm;
+
+    /**
+     * Set the search depth.
+     *
+     * @param int $depth The search depth.
+     *
+     * @return Algorithm Returns the minimax algorithm.
+     */
+    public function setDepth(int $depth): Algorithm;
+
+    /**
+     * Find the best move.
+     *
+     * @param Position $position The position to use.
+     *
+     * @return array|null Return the best move possible.
+     */
+    public function bestMove(Position $position): ?array;
+
+    /**
+     * Search the position for a minimax score.
+     *
+     * @param Position $position The position to search from.
+     * @param int      $alpha    The alpha declaration.
+     * @param int      $beta     The beta declaration.
+     * @param int      $depth    The search depth.
+     *
+     * @return array|int|null Return an array of the best move and score possible.
+     */
+    public function search(Position $position, int $alpha, int $beta, int $depth): int;
 }
