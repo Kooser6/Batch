30b1359d71a043b0045919e5e59737e2d11106ac
Nicholas <48822466+Kooser6@users.noreply.github.com>
2019-10-13 16:22:42 -0400

    Add logic.

--- src/Evaluation/CombinedEvaluator.php
+++ src/Evaluation/CombinedEvaluator.php
@@ -1,1 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Phoenix\Evaluation;
+
+use Phoenix\Batch\Evaluator;
+use Phoenix\Batch\Position;
+
+final class CombinedEvaluator implements Evaluator
+{
+    /**
+     * @var Evaluator[]
+     */
+    private $evaluators;
+
+    /**
+     * Consturct multiple evaluators.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(array $evaluators)
+    {
+        $this->evaluators = array_map(function (EvaluationInterface $evaluator): EvaluationInterface {
+            return $evaluator;
+        }, $evaluators);
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    public function evaluate(Position $position): int
+    {
+        $sum = 0;
+        foreach ($this->evaluators as $evaluator) {
+            $sum += $evaluator->evaluate($position);
+        }
+        return $sum;
+    }
+}
 
